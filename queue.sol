1858013c0f0ab4900ea1851aeecd0d6a40242925
SameNoName <61697143+SameNoName@users.noreply.github.com>
2021-10-15 08:00:34 +0500

    Add files via upload

---
+++ queue.sol
@@ -0,0 +1,24 @@
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+
+contract queue {
+    string[] public arr;
+
+    constructor() public {
+        require(tvm.pubkey() != 0, 101);
+        require(msg.pubkey() == tvm.pubkey(), 102);
+        tvm.accept();
+    }
+
+    function append (string s) public
+    {
+        tvm.accept();
+        arr.push(s);
+    }
+
+    function pop_first () public 
+    {
+        tvm.accept();
+        delete arr[0];
+    }
+}
